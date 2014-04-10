fca7bdce50e2da44b75fbd566662ca3a0301b853
Timo Lindemann <lindem@tilin.de>
2014-04-10 09:03:14 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# form-example
+
+This very simple, no-frills example shows how to do form submissions in general.
+
+The practical value is showing people who don't know how to do it how it works.
+
+----
+
+All source is UNLICENSEd. In other words, just use it and don't bother me with
+it except if you see blatant errors :-) See the file UNLICENSE for details. 
---
+++ app.js
@@ -0,0 +1,40 @@
+/**
+ * Module dependencies.
+ */
+
+var express = require('express'),
+    http = require('http'),
+    path = require('path');
+
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.favicon());
+
+// This is the middleware that parses a POST body to we can see the form fields.
+// It is usually included as a default.
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+
+app.get('/', function (req, res) {
+    res.render("index", {title: "form-example"});
+});
+
+// This is the view that is being invoked on form submit. It is responsible for
+// working with the form data (which itself is residing in req.body).
+// Look at views/index.jade to see how the template is being rendered
+// differently if there has been a form submission.
+app.post('/', function (req, res) {
+    // here, you can do anything with the form data. Process it, save it to a
+    // DB, you name it. I just log the req.body to console here (be sure to
+    // view it once).
+    console.log(req.body);
+    res.render("index", {title: "form-example", name: req.body.name});
+});
+
+http.createServer(app).listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
+});
